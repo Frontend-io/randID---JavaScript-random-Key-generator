3222ce4096dc75d1e17841e0224d9aced663012b
Jefferson Osagie Iyobosa <iyobosajefferson2@gmail.com>
2020-03-02 23:40:17 +0100

    Minor text correction & exported the function

--- randID.js
+++ randID.js
@@ -10,7 +10,7 @@ function randID(){
     const numerals = [0,1,2,3,4,5,6,7,8,9]
     let pushed = [] ;
     let outputRand = '';
-    let lengthOfOutput = 10; /* Default length is 10, you can change it to suite your need or supply length as a parameter when calling the function */
+    let lengthOfOutput = 10; /* Default length is 10, you can change it to suit your need or supply length as a parameter when calling the function */
 
 
     // If a parameter is passed, the first index is considered the outputLength length. This overrides any pre-hardcoded value within the function
@@ -55,3 +55,5 @@ function randID(){
     		return outputRand
     }
 }
+
+export default randID
